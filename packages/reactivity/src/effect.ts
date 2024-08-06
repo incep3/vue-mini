3ff489fe063dfb295aff2c8243dcf899b90829f2
Incep-3 <eros.zhang@betterwood.com>
2024-08-06 23:37:55 +0800

    feat(effect): 嵌套的effect 与 effect栈

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -34,12 +34,22 @@ export function trigger(target, key) {
 
 // 用一个全局变量存储被注册的副作用函数
 export let activeEffect
+// effect栈：处理嵌套的 effect
+const effectStack = []
 export function effect(fn) {
   const effectFn = () => {
     // 调用 cleanup 函数完成清除工作 重新收集依赖
     cleanup(effectFn)
     activeEffect = effectFn
-    fn()
+    // 在调用副作用函数之前将当前副作用函数压入栈中
+    effectStack.push(effectFn)
+    try {
+      fn()
+    } finally {
+      effectStack.pop()
+      // activeEffect 始终是栈顶元素
+      activeEffect = effectStack[effectStack.length - 1]
+    }
   }
   effectFn.deps = []
   // 调用时，副作用立即执行一次
