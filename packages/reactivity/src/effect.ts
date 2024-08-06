5f96957abb34be4fac6947d5ffa0259f7a055764
Incep-3 <eros.zhang@betterwood.com>
2024-08-07 00:03:52 +0800

    feat(effect): 避免无限递归循环

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -27,8 +27,20 @@ export function trigger(target, key) {
 
   const effects = depsMap.get(key)
 
+  const addToRun = (effects) => {
+    effects &&
+      effects.forEach((effectFn) => {
+        // 如果 trigger 触发执行的副作用函数与当前正在执行的副作用函数相同，则不触发执行
+        if (effectFn !== activeEffect) {
+          effectsToRun.add(effect)
+        }
+      })
+  }
+
   // 重新构造一个 Set 集合，防止无限执行（语言规范： 在调用 forEach 遍历 Set 集合时，如果一个值已经被访问过，但该值被删除并重新添加到集合，如果此时 forEach 遍历没有结束，那么该值会重新被访问）
-  const effectsToRun = new Set(effects)
+  const effectsToRun = new Set()
+  addToRun(effects)
+
   effectsToRun.forEach((fn: Function) => fn())
 }
 
