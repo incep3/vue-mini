b271bf69f54a833ecaa2a503f8ba805de32f5025
Incep-3 <eros.zhang@betterwood.com>
2024-08-06 23:18:16 +0800

    feat(effect): 分支切换与 cleanup

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -15,6 +15,9 @@ export function track(target, key) {
   }
 
   deps.add(activeEffect)
+  // deps 就是一个与当前副作用函数存在联系的依赖集合
+  // 将其添加到 activeEffect.deps 数组中
+  activeEffect.deps.push(deps)
 }
 
 export function trigger(target, key) {
@@ -23,11 +26,29 @@ export function trigger(target, key) {
 
   const effects = depsMap.get(key)
 
-  effects && effects.forEach((fn) => fn())
+  // 重新构造一个 Set 集合，防止无限执行（语言规范： 在调用 forEach 遍历 Set 集合时，如果一个值已经被访问过，但该值被删除并重新添加到集合，如果此时 forEach 遍历没有结束，那么该值会重新被访问）
+  const effectsToRun = new Set(effects)
+  effectsToRun.forEach((fn: Function) => fn())
 }
 
 export let activeEffect
 export function effect(fn) {
-  activeEffect = fn
-  fn()
+  const effectFn = () => {
+    // 调用 cleanup 函数完成清除工作 重新收集依赖
+    cleanup(effectFn)
+    activeEffect = effectFn
+    fn()
+  }
+  effectFn.deps = []
+  effectFn()
+}
+
+function cleanup(effectFn) {
+  for (let i = 0; i < effectFn.deps.length; i++) {
+    const deps = effectFn.deps[i]
+    // 将 effectFn 从依赖集合中移除
+    deps.delete(effectFn)
+  }
+  // 最后需要重置 effectFn.deps 数组
+  effectFn.deps.length = 0
 }
