8d213099095eb1ccfd28366a52af31b36ea9d712
Incep-3 <eros.zhang@betterwood.com>
2024-08-09 16:50:36 +0800

    feat(shared): add hasChanged & def helper function

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,6 +1,1 @@
-export const isObject = (val: unknown): val is Record<any, any> =>
-  val !== null && typeof val === 'object'
-
-const hasOwnProperty = Object.prototype.hasOwnProperty
-export const hasOwn = (target: object, key: string | symbol): boolean =>
-  hasOwnProperty.call(target, key)
+export * from './general'
