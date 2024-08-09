c861b1f58f5acebffd42ba26b4772295a6d17b84
Incep-3 <eros.zhang@betterwood.com>
2024-08-09 23:21:04 +0800

    feat(reactive): reactive 的判断条件

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,13 +1,57 @@
+import { isObject, toRawType } from '@vue/shared'
 import { baseHandlers } from './baseHandlers'
 import { ReactiveFlags } from './constants'
+import { warn } from './warning'
 
-export function reactive(target) {
-  return createReactiveObject(target, baseHandlers)
+export const reactiveMap = new WeakMap()
+
+enum TargetType {
+  INVALID = 0,
+  COMMON = 1,
 }
 
-function createReactiveObject(target, baseHandlers) {
-  const proxy = new Proxy(target, baseHandlers)
+function targetTypeMap(rawType) {
+  switch (rawType) {
+    case 'Object':
+      return TargetType.COMMON
+    default:
+      return TargetType.INVALID
+  }
+}
 
+function getTargetType(value) {
+  // isExtensible: Returns a value that indicates whether new properties can be added to an object.
+  return !Object.isExtensible(value)
+    ? TargetType.INVALID
+    : targetTypeMap(toRawType(value))
+}
+
+export function reactive(target) {
+  return createReactiveObject(target, baseHandlers, reactiveMap)
+}
+
+function createReactiveObject(target, baseHandlers, proxyMap) {
+  // only object can be made reactive
+  if (!isObject(target)) {
+    warn('target should be Object.')
+    return target
+  }
+  // target is already a Proxy, return it.
+  if (target[ReactiveFlags.RAW]) {
+    return target
+  }
+  // target already has corresponding Proxy
+  let existingProxy = proxyMap.get(target)
+  if (existingProxy) {
+    return existingProxy
+  }
+  // only specific value types can be observed.
+  const targetType = getTargetType(target)
+  if (targetType === TargetType.INVALID) {
+    return target
+  }
+  const proxy = new Proxy(target, baseHandlers)
+  proxyMap.set(target, proxy)
   return proxy
 }
 
---
+++ packages/reactivity/src/warning.ts
@@ -0,0 +1,3 @@
+export function warn(msg, ...args) {
+  console.warn(`[Vue warn] ${msg}`, ...args)
+}
